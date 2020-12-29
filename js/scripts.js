b48b21e7bc3c36a4c60a1134c1ec4c1c42bfd897
Dan Orban <dtorban@umn.edu>
2020-12-29 10:39:22 -0600

    Updating CV.

--- js/scripts.js
+++ js/scripts.js
@@ -51,7 +51,7 @@
     });
 
     // Create timeline
-    $('#experience-timeline').each(function() {
+    $('.experience-timeline').each(function() {
 
         $this = $(this); // Store reference to this
         $userContent = $this.children('div'); // user content
